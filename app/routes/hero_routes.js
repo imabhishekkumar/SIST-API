20d08c8368aacd3f7594decd1c41492531f00fee
Abhishek Kumar <abhishek.kumar.7679@gmail.com>
2020-03-17 18:06:37 +0530

    initial push

--- app/routes/hero_routes.js
+++ app/routes/hero_routes.js
@@ -1,12 +1,15 @@
 module.exports = function(app,db){
-    app.post('/hero/:id', (req, res) => {
+    app.get('/hero/:id', (req, res) => {
         console.log(req.body);
         var fs = require('../../data.json');
         res.send(fs[req.params.id])
     });
-    app.post('/heros', (req, res) => {
+    app.get('/heros', (req, res) => {
         console.log(req.body);
         var fs = require('../../data.json');
         res.send(fs)
     });
+
+
+
 }
