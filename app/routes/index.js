ded6233f685c33ce57701970a5c0f4f15d0a7289
Abhishek Kumar <abhishek.kumar.7679@gmail.com>
2020-03-18 10:35:36 +0530

    sport_news

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,6 @@
 const latestNews = require('./latest_news.js');
+const sportNews = require('./sport_news.js');
 module.exports = function(app,db){
     latestNews(app,db);
+    sportNews(app,db);
 }
